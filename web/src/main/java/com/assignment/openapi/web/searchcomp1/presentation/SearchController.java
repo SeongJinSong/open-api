a859fbe19de5c03f4fadfc58c56a783982778494
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-20 23:27:11 +0900

    feat : controller 계층에 facade 패턴 적용

--- web/src/main/java/com/assignment/openapi/web/searchcomp1/presentation/SearchController.java
+++ web/src/main/java/com/assignment/openapi/web/searchcomp1/presentation/SearchController.java
@@ -1,11 +1,7 @@
 package com.assignment.openapi.web.searchcomp1.presentation;
 
-import com.assignment.openapi.core.redis.service.QueryCountService;
-import com.assignment.openapi.web.searchcomp1.contents.Blog;
 import com.assignment.openapi.core.search.domain.SearchRank;
-import com.assignment.openapi.web.searchcomp1.service.SearchHistoryService;
-import com.assignment.openapi.web.searchcomp1.service.SearchRankService;
-import com.assignment.openapi.web.searchcomp1.service.SearchService;
+import com.assignment.openapi.web.searchcomp1.application.SearchFacade;
 import com.assignment.openapi.web.searchcomp1.presentation.dto.SearchRequest;
 import com.assignment.openapi.web.searchcomp1.presentation.dto.SearchResponse;
 import com.assignment.openapi.web.wrapper.ResponseWrapper;
@@ -26,27 +22,13 @@ import java.util.List;
 @RequestMapping(path = "/v2/search/blog")
 @RequiredArgsConstructor
 @RestController
-public class SearchController {
-    private final SearchService searchService;
-    private final SearchRankService searchRankService;
-    private final SearchHistoryService searchHistoryService;
-    private final QueryCountService queryCountService;
-
+public class SearchController<T> {
+    private final SearchFacade searchFacade;
     @GetMapping
-    public ResponseEntity<ResponseWrapper<SearchResponse<Blog>>> getContentsList(
+    public ResponseEntity<ResponseWrapper<SearchResponse<T>>> getContentsList(
             HttpServletRequest httpservletRequest, @Valid SearchRequest request){
-        // TODO 카카오 api에서 실패하는 경우 네이버 api를 사용하는 것을 구현하기위해 MSA에서 fault tolerance로 많이 사용하는tolaresilience4j를 적용해보자.
-        searchHistoryService.saveRequest(request);
-
-        //레디스에 카운트 설정
-        //TODO : LongAdder, Accumulator 적용해보기
-        queryCountService.inCreateCount(request.getQuery());
-
-        //TODO 조회와 삽입이 분리되면 동시성 문제가 발생할 수 있어 lua-script 기반으로 돌리는 방법도 찾아보자
-        searchRankService.addSearchRequest(request.getQuery());
-        return ResponseWrapper.ok(searchService.getContentsList(httpservletRequest.getRequestURI(), makeQueryString(request)), "success");
+        return ResponseWrapper.ok(searchFacade.getContentsList(httpservletRequest, request), "success");
     }
-
     /**
      * DB에 저장된 History를 검색어 기반으로 그루핑해 오더링한 결과를 리턴한다.
      *
@@ -55,14 +37,15 @@ public class SearchController {
      */
     @GetMapping("/rank")
     public ResponseEntity<ResponseWrapper<Page<SearchRank>>> getPopularSearchWord(Pageable pageable){
-        return ResponseWrapper.ok(searchHistoryService.getPopularSearchWord(pageable), "success");
+        return ResponseWrapper.ok(searchFacade.getPopularSearchWord(pageable), "success");
     }
 
+    /**
+     * Redis ZSET 을 활용하여 오더링한 결과를 리턴한다.
+     * @return
+     */
     @GetMapping("/rank-new")
     public ResponseEntity<ResponseWrapper<List<SearchRank>>> searchRankList(){
-        return ResponseWrapper.ok(searchRankService.searchRankList(), "success");
-    }
-    private String makeQueryString(SearchRequest request){
-        return "query="+request.getQuery()+"&sort="+request.getSort()+"&page="+request.getPage()+"&size="+request.getSize();
+        return ResponseWrapper.ok(searchFacade.searchRankList(), "success");
     }
 }
--- web/src/main/java/com/assignment/openapi/web/searchcomp1/service/SearchService.java
+++ web/src/main/java/com/assignment/openapi/web/searchcomp1/service/SearchService.java
@@ -7,7 +7,7 @@ import com.assignment.openapi.web.searchcomp1.presentation.dto.SearchResponse;
 /**
  * 검색과 관련된 동작을 정의한 인터페이스
  */
-public interface SearchService {
-    SearchResponse<Blog> getContentsList(
+public interface SearchService<T> {
+    SearchResponse<T> getContentsList(
             String uri, String queryString);
 }
