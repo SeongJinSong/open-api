a3fb78c115b73d53cdc2a32756a37d5cd3fc0e7c
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-21 19:20:46 +0900

    feat : searchRequest, searchResponse를 부모로 회사별 상속받게함

--- web/src/main/java/com/assignment/openapi/web/searchcomp1/presentation/dto/SearchComp1Response.java
+++ web/src/main/java/com/assignment/openapi/web/searchcomp1/presentation/dto/SearchComp1Response.java
@@ -1,5 +1,6 @@
 package com.assignment.openapi.web.searchcomp1.presentation.dto;
 
+import com.assignment.openapi.web.apiutil.SearchResponse;
 import lombok.*;
 
 import java.io.Serializable;
@@ -11,7 +12,7 @@ import java.util.List;
 @Builder
 @ToString
 //TODO 각각의 검색 결과를 캐싱할 필요가 있을까? 다른 알고리즘 찾아보자
-public class SearchComp1Response<T> implements Serializable {
+public class SearchComp1Response<T> extends SearchResponse implements Serializable {
     private Meta meta;
     private List<T> documents;
 }
