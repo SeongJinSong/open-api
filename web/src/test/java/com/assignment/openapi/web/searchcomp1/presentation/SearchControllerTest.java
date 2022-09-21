fb5ac8ba5b2ed8c0f67708d8b714fa377b650b52
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-21 22:32:02 +0900

    fix : 테스트 작성중

--- web/src/test/java/com/assignment/openapi/web/searchcomp1/presentation/SearchControllerTest.java
+++ web/src/test/java/com/assignment/openapi/web/searchcomp1/presentation/SearchControllerTest.java
@@ -1,17 +1,33 @@
 package com.assignment.openapi.web.searchcomp1.presentation;
 
+import com.assignment.openapi.core.error.ErrorCode;
+import com.assignment.openapi.core.error.exception.NetworkException;
+import com.assignment.openapi.web.apiutil.SearchResponse;
+import com.assignment.openapi.web.searchcomp1.api.Comp1WebClientRequester;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
 @Transactional
@@ -20,6 +36,9 @@ class SearchControllerTest {
     @Autowired
     //IntelliJ 버전 이슈로 IDE 버그 있음(https://stackoverflow.com/questions/73085049/cant-autowired-mockmvc-using-webmvctest)
     private MockMvc mockMvc;
+
+//    @MockBean
+//    Comp1WebClientRequester comp1WebClientRequester;
 
     @Order(1)
     @DisplayName("1 API 조회 IntegrationTest")
@@ -41,7 +60,7 @@ class SearchControllerTest {
         @DisplayName("2-1. 블로그 리스트 조회 V2")
         @Test
         void getBlogListV2() throws Exception {
-            mockMvc.perform(get("/api/v2/search/blog?query=https://brunch.co.kr/@tourism 여행&page=1&size=10&sort=accuracy")
+            mockMvc.perform(get("/api/v2/search/blog?query=https://brunch.co.kr/@tourism 여행&page=1&size=10&sort=recency")
                             .header("host", "localhost:8080")
                             .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
                             .accept(MediaType.APPLICATION_JSON)
@@ -81,6 +100,8 @@ class SearchControllerTest {
     }
     @Order(3)
     @DisplayName("3. API 재처리 IntegrationTest")
+    @TestMethodOrder(MethodOrderer.DisplayName.class)
+    @Nested
     class V3ApiTest{
         @DisplayName("1. 블로그 리스트 조회 첫번째 정상")
         @Test
@@ -93,16 +114,21 @@ class SearchControllerTest {
                     .andExpect(status().isOk())
                     .andDo(print());
         }
-        @DisplayName("2. 블로그 리스트 조회 첫번째 에러로 두번째로 재시도")
+        @DisplayName("2. 블로그 리스트 조회 첫번째 에러로 두번째로 재시도 테스트 코드 작성중 재출")
         @Test
         void getRetry2() throws Exception {
-            mockMvc.perform(get("/api/v3/search/blog?query=여행&page=1&size=10&sort=accuracy")
+            WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
+//            doThrow(RuntimeException.class).when(comp1WebClientRequester).getWebClient(any());
+//            when(comp1WebClientRequester.getWebClient(any())).thenReturn(responseSpec);
+            MvcResult result = mockMvc.perform(get("/api/v3/search/blog?query=여행&page=1&size=10&sort=recency")
                             .header("host", "localhost:8080")
                             .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
                             .accept(MediaType.APPLICATION_JSON)
                     )
                     .andExpect(status().isOk())
-                    .andDo(print());
+                    .andDo(print())
+                    .andReturn();
+            log.info(result.getResponse().getContentAsString());
         }
     }
 }
