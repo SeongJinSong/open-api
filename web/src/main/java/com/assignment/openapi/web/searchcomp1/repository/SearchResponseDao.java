03a09d76a6dc6ee57bfb9d8809db6c8329935eba
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-21 19:03:50 +0900

    fix : comp2 api 레디스 캐싱 버그 수정

--- web/src/main/java/com/assignment/openapi/web/searchcomp1/repository/SearchResponseDao.java
+++ web/src/main/java/com/assignment/openapi/web/searchcomp1/repository/SearchResponseDao.java
@@ -1,7 +1,6 @@
 package com.assignment.openapi.web.searchcomp1.repository;
 
 import com.assignment.openapi.core.redis.repository.ResponseDao;
-import com.assignment.openapi.web.searchcomp1.presentation.dto.SearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -19,7 +18,7 @@ public class SearchResponseDao implements ResponseDao {
         redisTemplate.expire(key, 10, TimeUnit.SECONDS);
     }
 
-    public SearchResponse findById(String key){
-        return (SearchResponse) redisTemplate.opsForValue().get(key);
+    public Object findById(String key){
+        return redisTemplate.opsForValue().get(key);
     }
 }
