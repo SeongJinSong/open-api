7e1f62e8cfff8e1a69060f377d2c7037300d33ea
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-21 21:03:21 +0900

    feat : 상수 전부 util로 이동

--- web/src/main/java/com/assignment/openapi/web/searchcomp1/api/Comp1ApiService.java
+++ web/src/main/java/com/assignment/openapi/web/searchcomp1/api/Comp1ApiService.java
@@ -3,7 +3,10 @@ package com.assignment.openapi.web.searchcomp1.api;
 import com.assignment.openapi.core.error.ErrorCode;
 import com.assignment.openapi.core.error.exception.NetworkException;
 import com.assignment.openapi.web.apiutil.ApiService;
+import com.assignment.openapi.web.apiutil.SearchResponse;
 import com.assignment.openapi.web.searchcomp1.presentation.dto.SearchComp1Response;
+import com.assignment.openapi.web.searchcomp2.api.Comp2WebClientRequester;
+import com.assignment.openapi.web.searchcomp2.presentation.dto.SearchComp2Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,10 +17,21 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class Comp1ApiService implements ApiService {
-    private final Comp1WebClientRequester webClientRequester;
+    private final Comp1WebClientRequester comp1WebClientRequester;
+    private final Comp2WebClientRequester comp2WebClientRequester;
     public SearchComp1Response get(String host, String urlTemplate) {
-        return Optional.ofNullable(webClientRequester.getWebClient(host+urlTemplate)
+        return Optional.ofNullable(comp1WebClientRequester.getWebClient(host+urlTemplate)
                         .bodyToMono(SearchComp1Response.class).block())
+                .orElseThrow(()->{throw new NetworkException(ErrorCode.REQUEST_TIMEOUT_EXCEPTION);});
+    }
+    public SearchResponse getRetry(String host, String urlTemplate, String host2, String urlTemplate2) {
+        return Optional.ofNullable(comp1WebClientRequester.getWebClient(host+urlTemplate)
+                        .bodyToMono(SearchResponse.class)
+                        .onErrorResume(error -> {
+                            log.info("comp1ApiCall Error = {}", error);
+                            return comp2WebClientRequester.getWebClient(host+urlTemplate2).bodyToMono(SearchComp2Response.class);
+                        }).block()
+                )
                 .orElseThrow(()->{throw new NetworkException(ErrorCode.REQUEST_TIMEOUT_EXCEPTION);});
     }
 }
