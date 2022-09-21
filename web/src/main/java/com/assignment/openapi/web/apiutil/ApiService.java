e71426a43988d4cdb5e1cbee1eb2aca99bd70281
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-21 17:47:17 +0900

    refactor : apiservice, webclientrequester interface 분리

--- web/src/main/java/com/assignment/openapi/web/apiutil/ApiService.java
+++ web/src/main/java/com/assignment/openapi/web/apiutil/ApiService.java
@@ -1,23 +1,7 @@
 package com.assignment.openapi.web.apiutil;
 
-import com.assignment.openapi.core.error.ErrorCode;
-import com.assignment.openapi.core.error.exception.NetworkException;
 import com.assignment.openapi.web.searchcomp1.presentation.dto.SearchResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class ApiService {
-    private final WebClientRequester webClientRequester;
-    //TODO searchResponse에 의존하는것 수정 필요
-    public SearchResponse get(String host, String urlTemplate) {
-        return Optional.ofNullable(webClientRequester.getWebClient(host+urlTemplate)
-                        .bodyToMono(SearchResponse.class).block())
-                .orElseThrow(()->{throw new NetworkException(ErrorCode.REQUEST_TIMEOUT_EXCEPTION);});
-    }
+public interface ApiService {
+    SearchResponse get(String host, String urlTemplate);
 }
---
+++ web/src/main/java/com/assignment/openapi/web/searchcomp1/api/Comp1WebClientRequester.java
@@ -0,0 +1,39 @@
+package com.assignment.openapi.web.searchcomp1.api;
+
+import com.assignment.openapi.web.apiutil.WebClientRequester;
+import io.netty.handler.logging.LogLevel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.client.HttpClientRequest;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+@Component
+public class Comp1WebClientRequester implements WebClientRequester {
+    @Autowired
+    HttpServletRequest request;
+    private final HttpClient httpClient = HttpClient
+            .create()
+            .wiretap(HttpClient.class.getCanonicalName(), LogLevel.DEBUG, AdvancedByteBufFormat.SIMPLE, StandardCharsets.UTF_8);
+    private final WebClient webClient = WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
+    public WebClient.ResponseSpec getWebClient(String uri){
+        return webClient.get()
+                .uri(uri)
+                .httpRequest(clientHttpRequest -> {
+                    HttpClientRequest httpRequest = clientHttpRequest.getNativeRequest();
+                    httpRequest.responseTimeout(Duration.ofSeconds(5));
+                })
+                .header("Content-Type", request.getHeader("Content-Type")!=null?request.getHeader("Content-Type"):"application/x-www-form-urlencoded")
+                .header( "Accept", request.getHeader("Accept")!=null?request.getHeader("Accept"):"application/json")
+                .header("Authorization", request.getHeader("Authorization")!=null?request.getHeader("Authorization"):"KakaoAK 9ccac738a217f2aa9d006c01900809cc")
+                .retrieve();
+    }
+}
